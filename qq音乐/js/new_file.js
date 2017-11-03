89a42b465893864878b45db2e4ec1872985195ca
崮生 <2234839456@qq.com>
2017-11-03 17:03:13 +0800

    Add files via upload

--- qq音乐/js/new_file.js
+++ qq音乐/js/new_file.js
@@ -1,8 +1,9 @@
 window.onload=function (){
-right_zhu=document.getElementById("right").innerHTML;
-right_bendi=document.getElementById("bendi").innerHTML;
-right_mv=document.getElementById("MV").innerHTML;
-
+right_zhu=document.getElementById("right").innerHTML;	document.getElementById("right").innerHTML="";
+right_bendi=document.getElementById("bendi").innerHTML;	document.getElementById("bendi").innerHTML="";
+right_mv=document.getElementById("MV").innerHTML;		document.getElementById("MV").innerHTML="";
+right_个性电台=document.getElementById("个性电台").innerHTML;document.getElementById("个性电台").innerHTML="";
+jiemian(right_zhu)
 	setInterval(lunbo,2500);//设置轮播
 	
 	setInterval(jindu,1000);//刷新下面进度条
